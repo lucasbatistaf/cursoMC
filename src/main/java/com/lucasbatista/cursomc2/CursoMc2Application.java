b700b74f57e981bc5baf89621320e718143d3284
Lucas Batista <lucaasbatista182@hotmail.com>
2022-05-16 16:03:33 -0300

    Projeto Criado

---
+++ src/main/java/com/lucasbatista/cursomc2/CursoMc2Application.java
@@ -0,0 +1,13 @@
+package com.lucasbatista.cursomc2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursoMc2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CursoMc2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/lucasbatista/cursomc2/CursoMc2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucasbatista.cursomc2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursoMc2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
