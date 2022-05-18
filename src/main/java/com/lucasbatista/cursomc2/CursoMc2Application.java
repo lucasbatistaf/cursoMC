0ce28352ad91e3ce6c0140db6d2de7d3163dd8d7
Lucas Batista <lucaasbatista182@hotmail.com>
2022-05-18 11:48:48 -0300

    Creating an instance operation

--- src/main/java/com/lucasbatista/cursomc2/CursoMc2Application.java
+++ src/main/java/com/lucasbatista/cursomc2/CursoMc2Application.java
@@ -1,13 +1,31 @@
 package com.lucasbatista.cursomc2;
 
+import com.lucasbatista.cursomc2.domain.Category;
+import com.lucasbatista.cursomc2.repository.CategoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Arrays;
+
 @SpringBootApplication
-public class CursoMc2Application {
+public class CursoMc2Application  implements CommandLineRunner {
+
+    @Autowired
+    private CategoryRepository categoryRepository;
 
     public static void main(String[] args) {
         SpringApplication.run(CursoMc2Application.class, args);
     }
 
+    @Override
+    public void run(String... args) throws Exception {
+
+        Category cat1 = new Category(null, "Informatica");
+        Category cat2 = new Category(null, "Escritorio");
+
+        categoryRepository.saveAll(Arrays.asList(cat1, cat2));
+
+    }
 }
