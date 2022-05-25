a5f1fa9c1482639cc5524628cf08deea9e15cc82
Lucas Batista <lucaasbatista182@hotmail.com>
2022-05-25 15:42:02 -0300

    Adjustments and Category PUT updated

--- src/main/java/com/lucasbatista/cursomc2/resources/CategoryResource.java
+++ src/main/java/com/lucasbatista/cursomc2/resources/CategoryResource.java
@@ -2,6 +2,7 @@ package com.lucasbatista.cursomc2.resources;
 
 import com.lucasbatista.cursomc2.domain.Category;
 import com.lucasbatista.cursomc2.service.CategoryService;
+import org.apache.coyote.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -28,6 +29,13 @@ public class CategoryResource {
         URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
                 .path("/{id}").buildAndExpand(obj.getId()).toUri();
         return ResponseEntity.created(uri).build();
+    }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.PUT)
+    public ResponseEntity<Void> update(@RequestBody Category obj, @PathVariable Integer id){
+        obj.setId(id);
+        obj = service.update(obj);
+        return ResponseEntity.noContent().build();
     }
 
 }
